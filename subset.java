9fd6c8bef88b3a24c564f722078f421fa12d2014
malarmagalcse <malarmagal.ncse@ksrce.ac.in>
2016-07-22 14:42:29 +0530

    Create subset.java

---
+++ subset.java
@@ -0,0 +1,16 @@
+import java.util.Arrays; 
+import java.util.HashSet; 
+import java.util.Set; 
+
+public class MainClass { 
+
+public static void main(String[] a) { 
+String elements[] = { "A", "B", "C", "D", "E" }; 
+Set set = new HashSet(Arrays.asList(elements)); 
+
+elements = new String[] { "A", "B", "C" }; 
+Set set2 = new HashSet(Arrays.asList(elements)); 
+
+System.out.println(set.containsAll(set2)... 
+} 
+}
